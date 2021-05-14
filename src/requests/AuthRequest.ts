eda323eebce9bc0776cf7a00d028bb51e92ca71c
ThayalanGR <grthayalan18@gmail.com>
2021-05-14 13:56:54 +0530

    requests architecture changes

--- src/requests/AuthRequest.ts
+++ src/requests/AuthRequest.ts
@@ -10,7 +10,6 @@ import {
     ROUTES,
 } from '@sellerspot/universal-types';
 
-import { apiService } from 'services/services';
 import BaseRequest from './BaseRequest';
 
 export default class AuthRequest extends BaseRequest {
@@ -19,38 +18,38 @@ export default class AuthRequest extends BaseRequest {
     }
 
     async signupTenant(payload: ISignupTenantRequest): Promise<ISignupTenantResponse> {
-        return <ISignupTenantResponse>await apiService.request({
-            url: this.getUrl(ROUTES.AUTH.SIGN_UP),
+        return <ISignupTenantResponse>await this.request({
+            url: ROUTES.AUTH.SIGN_UP,
             method: REQUEST_METHOD.POST,
             payload,
         });
     }
 
     async signInTenant(payload: ISigninTenantRequest): Promise<ISigninTenantResponse> {
-        return <ISigninTenantResponse>await apiService.request({
-            url: this.getUrl(ROUTES.AUTH.SIGN_IN),
+        return <ISigninTenantResponse>await this.request({
+            url: ROUTES.AUTH.SIGN_IN,
             method: REQUEST_METHOD.POST,
             payload,
         });
     }
 
     async checkIsUserAuthenticated(domainName: string): Promise<ICurrentUserResponse> {
-        return <ICurrentUserResponse>await apiService.request({
-            url: `${this.getUrl(`${ROUTES.AUTH.CURRENT_USER}?domain=${domainName}`)}`,
+        return <ICurrentUserResponse>await this.request({
+            url: `${ROUTES.AUTH.CURRENT_USER}?domain=${domainName}`,
             method: REQUEST_METHOD.GET,
         });
     }
 
     async checkDomainAvailability(domainName: string): Promise<ICheckDomainAvailabilityResponse> {
-        return await apiService.request({
-            url: `${this.getUrl(ROUTES.AUTH.CHECK_DOMAIN_AVAILABILITY)}?domain=${domainName}`,
+        return await this.request({
+            url: `${ROUTES.AUTH.CHECK_DOMAIN_AVAILABILITY}?domain=${domainName}`,
             method: REQUEST_METHOD.GET,
         });
     }
 
     async identifyStore(domainName: string): Promise<IIdentifyStoreResponse> {
-        return <IIdentifyStoreResponse>await apiService.request({
-            url: this.getUrl(`${ROUTES.AUTH.IDENTIFY_STORE}?domain=${domainName}`),
+        return <IIdentifyStoreResponse>await this.request({
+            url: `${ROUTES.AUTH.IDENTIFY_STORE}?domain=${domainName}`,
             method: REQUEST_METHOD.GET,
         });
     }
