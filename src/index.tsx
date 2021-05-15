0fb7746351cc9e1b6bd9a2d67d2c45a2850b011d
ThayalanGR <grthayalan18@gmail.com>
2021-05-15 16:13:24 +0530

    theme provider restrcturing done

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@sellerspot/universal-components';
 
 import { App } from 'layouts/App/App';
-import { COLORS, FONT_SIZE } from 'config/theme';
 
 ReactDOM.render(
     <BrowserRouter>
-        <ThemeProvider colors={COLORS} fontSizes={FONT_SIZE}>
-            <App />
-        </ThemeProvider>
+        <App />
     </BrowserRouter>,
     document.getElementById('root'),
 );
--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -1,9 +1,13 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import cn from 'classnames';
-
-import { AppPreloader, Logo, LogoText, Notify } from '@sellerspot/universal-components';
-import { initializeGlobalConfig } from 'config/globalConfig';
+import {
+    AppPreloader,
+    Logo,
+    LogoText,
+    Notify,
+    ThemeProvider,
+} from '@sellerspot/universal-components';
 
 import { ROUTES } from 'config/routes';
 import { CONFIG } from 'config/config';
@@ -11,6 +15,8 @@ import { CONFIG } from 'config/config';
 import '../../styles/core.scss';
 import styles from './app.module.scss';
 
+import { initializeGlobalConfig } from 'config/globalConfig';
+import { COLORS, FONT_SIZE } from 'config/theme';
 import { SignUp } from 'pages/SignUp/SignUp';
 import { SignIn } from 'pages/SignIn/SignIn';
 import { IdentifyStore } from 'pages/IdentifyStore/IdentifyStore';
@@ -36,46 +42,48 @@ export const App = (): ReactElement => {
     };
 
     return (
-        <div className={styles.appWrapper}>
-            {isLoading ? (
-                <AppPreloader />
-            ) : (
-                <div className={cn(styles.appContainer)}>
-                    <div className={styles.logoWrapper} onClick={logoClickHandler}>
-                        <Logo className={styles.logoicon} />
-                        <LogoText className={styles.logoText} />
+        <ThemeProvider colors={COLORS} fontSizes={FONT_SIZE}>
+            <div className={styles.appWrapper}>
+                {isLoading ? (
+                    <AppPreloader />
+                ) : (
+                    <div className={cn(styles.appContainer)}>
+                        <div className={styles.logoWrapper} onClick={logoClickHandler}>
+                            <Logo className={styles.logoicon} />
+                            <LogoText className={styles.logoText} />
+                        </div>
+                        <Switch>
+                            {/* all other routes should be nested above this route because it is '/' route hence should be placed atlast */}
+                            <Route path={ROUTES.SIGN_UP}>
+                                <SignUp />
+                            </Route>
+                            <Route path={ROUTES.CACHED_SIGN_IN}>
+                                <CachedSignIn />
+                            </Route>
+                            <Route path={ROUTES.IDENTIFY_STORE}>
+                                <IdentifyStore />
+                            </Route>
+                            <Route path={ROUTES.SIGN_IN}>
+                                <SignIn />
+                            </Route>
+                            <Route path={ROUTES.FORGOT_STORE_URL}>
+                                <ForgotStoreUrl />
+                            </Route>
+                            <Route path={ROUTES.FORGOT_PASSWORD}>
+                                <ForgotPassword />
+                            </Route>
+                            <Route path={ROUTES.RESET_PASSWORD}>
+                                <ResetPassword />
+                            </Route>
+                            <Route>
+                                <Redirect to={ROUTES.CACHED_SIGN_IN} />
+                            </Route>
+                        </Switch>
                     </div>
-                    <Switch>
-                        {/* all other routes should be nested above this route because it is '/' route hence should be placed atlast */}
-                        <Route path={ROUTES.SIGN_UP}>
-                            <SignUp />
-                        </Route>
-                        <Route path={ROUTES.CACHED_SIGN_IN}>
-                            <CachedSignIn />
-                        </Route>
-                        <Route path={ROUTES.IDENTIFY_STORE}>
-                            <IdentifyStore />
-                        </Route>
-                        <Route path={ROUTES.SIGN_IN}>
-                            <SignIn />
-                        </Route>
-                        <Route path={ROUTES.FORGOT_STORE_URL}>
-                            <ForgotStoreUrl />
-                        </Route>
-                        <Route path={ROUTES.FORGOT_PASSWORD}>
-                            <ForgotPassword />
-                        </Route>
-                        <Route path={ROUTES.RESET_PASSWORD}>
-                            <ResetPassword />
-                        </Route>
-                        <Route>
-                            <Redirect to={ROUTES.CACHED_SIGN_IN} />
-                        </Route>
-                    </Switch>
-                </div>
-            )}
-            {/* globalcomponents */}
-            <Notify />
-        </div>
+                )}
+                {/* globalcomponents */}
+                <Notify />
+            </div>
+        </ThemeProvider>
     );
 };
